5cec891fcab2a41977d0a604ad4ea025b90b7122
Jordy <jordyfgomes@gmail.com>
2015-05-04 22:11:04 -0300

    nice

--- src/objects/Quarto.cpp
+++ src/objects/Quarto.cpp
@@ -16,31 +16,31 @@ std::vector<Mesh *> Quarto::meshes() const
 }
 Quarto::Quarto()
 {
-    m_meshes.push_back(new Square(vec2(9*TAM_LADRILHO,27*TAM_LADRILHO),vec3(1.0f,1.0f,1.0f)) );
-    LASTOF(m_meshes)->transform()->rotate(90.0f,1,0,0);
+//    m_meshes.push_back(new Square(vec2(9*TAM_LADRILHO,27*TAM_LADRILHO),vec3(1.0f,1.0f,1.0f)) );
+//    LASTOF(m_meshes)->transform()->rotate(90.0f,1,0,0);
 
-    m_meshes.push_back(new Square(vec2(9*TAM_LADRILHO,27*TAM_LADRILHO),vec3(1.0f,1.0f,1.0f)) );
-    LASTOF(m_meshes)->transform()->translate(0,ALTURA,0);
-    LASTOF(m_meshes)->transform()->rotate(90.0f,1,0,0);
+//    m_meshes.push_back(new Square(vec2(9*TAM_LADRILHO,27*TAM_LADRILHO),vec3(1.0f,1.0f,1.0f)) );
+//    LASTOF(m_meshes)->transform()->translate(0,ALTURA,0);
+//    LASTOF(m_meshes)->transform()->rotate(90.0f,1,0,0);
 
 
     m_meshes.push_back(new Grid(vec3(0,0.0001f,0),vec2(TAM_LADRILHO,TAM_LADRILHO),27,
-                                vec3(0.f,0.f,0.f)));
+                                vec3(1.f,1.f,1.f)));
     //PAREDES
-    m_meshes.push_back(new Square(vec2(27*TAM_LADRILHO,ALTURA),vec3(1.0f,1.0f,1.0f)) );
-    LASTOF(m_meshes)->transform()->translate(-9*TAM_LADRILHO/2,ALTURA/2,0);
-    LASTOF(m_meshes)->transform()->rotate(90.0f,0,1,0);
+//    m_meshes.push_back(new Square(vec2(27*TAM_LADRILHO,ALTURA),vec3(1.0f,1.0f,1.0f)) );
+//    LASTOF(m_meshes)->transform()->translate(-9*TAM_LADRILHO/2,ALTURA/2,0);
+//    LASTOF(m_meshes)->transform()->rotate(90.0f,0,1,0);
 
 
-    m_meshes.push_back(new Square(vec2(27*TAM_LADRILHO,ALTURA),vec3(1.0f,1.0f,1.0f)) );
-    LASTOF(m_meshes)->transform()->translate(9*TAM_LADRILHO/2,ALTURA/2,0);
-    LASTOF(m_meshes)->transform()->rotate(-90.0f,0,1,0);
+//    m_meshes.push_back(new Square(vec2(27*TAM_LADRILHO,ALTURA),vec3(1.0f,1.0f,1.0f)) );
+//    LASTOF(m_meshes)->transform()->translate(9*TAM_LADRILHO/2,ALTURA/2,0);
+//    LASTOF(m_meshes)->transform()->rotate(-90.0f,0,1,0);
 
-    m_meshes.push_back(new Square(vec2(9*TAM_LADRILHO,ALTURA),vec3(1.0f,1.0f,1.0f)) );
-    LASTOF(m_meshes)->transform()->translate(0,ALTURA/2,27*TAM_LADRILHO/2);
+//    m_meshes.push_back(new Square(vec2(9*TAM_LADRILHO,ALTURA),vec3(1.0f,1.0f,1.0f)) );
+//    LASTOF(m_meshes)->transform()->translate(0,ALTURA/2,27*TAM_LADRILHO/2);
 
-    m_meshes.push_back(new Square(vec2(9*TAM_LADRILHO,ALTURA),vec3(1.0f,1.0f,1.0f)) );
-    LASTOF(m_meshes)->transform()->translate(0,ALTURA/2,-27*TAM_LADRILHO/2);
+//    m_meshes.push_back(new Square(vec2(9*TAM_LADRILHO,ALTURA),vec3(1.0f,1.0f,1.0f)) );
+//    LASTOF(m_meshes)->transform()->translate(0,ALTURA/2,-27*TAM_LADRILHO/2);
 
 
     m_meshes.push_back(new Mesh("Rack.obj",vec3(0.2,0.3,0.6)));
